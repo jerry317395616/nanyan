85e6163257cda5fad0d2e86c853bae5c669eca4e
jerry317395616 <317395616@qq.com>
2024-08-02 23:44:34 +0800

    feat: Initialize App

---
+++ nanyan/nanyan/doctype/ly_points_record/ly_points_record.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, nanyan and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Ly Points Record", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ nanyan/nanyan/doctype/ly_user/ly_user.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, nanyan and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Ly User", {
+// 	refresh(frm) {
+
+// 	},
+// });
